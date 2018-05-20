d162f255a77050c25f965dfd75f7f7a4de1285bd
Austin Pierce <AustinPierce@gmail.com>
2018-05-20 16:33:36 -0400

    Syle errors and messages partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
    # render plain: params[:article].inspect #show what was just created
     @article = Article.new(article_params) #white list values, create method #article_params
     if @article.save
-      flash[:notice] = "Article was successfully created"
+      flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render :new
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
   
   def update
       if @article.update(article_params)
-        flash[:notice] = "Article was successfully updated"
+        flash[:success] = "Article was successfully updated"
         redirect_to article_path(@article)
       else
         render :edit
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
   
   def destroy # action
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+    flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
   
