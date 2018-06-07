46030a89c46082c832c4a3ef3dfdf8ddf7a19449
Austin Pierce <AustinPierce@gmail.com>
2018-06-06 22:00:25 -0400

    Create category test for admin. Require admin for category

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -21,4 +21,13 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
   
+  test "should redirect create when admin not logged in" do
+    # this works because in the setup of this test page there is no logged in user
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category: {name:"sports"}}
+    end
+    assert_redirected_to categories_path
+  end
+  
+  
 end
