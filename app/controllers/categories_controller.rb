0c62f5e41da16418f5330bea2931e4d95c20cd7a
Austin Pierce <AustinPierce@gmail.com>
2018-06-07 23:13:32 -0400

    Add edit category functionality. Limit edit to admins

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
+  before_action :find_category, only: [:show, :edit, :update]
   
   def index
    @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -20,14 +21,31 @@ class CategoriesController < ApplicationController
   end
   
   def show
-    @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
   
+  def edit
+  
+  end
+  
+  def update
+    oldName = @category.name
+    if @category.update(category_params)
+      flash[:success] = "Category name was successfully updated from #{oldName} to #{@category.name}"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+    
   private 
   
   def category_params
     params.require(:category).permit(:name)
+  end
+    
+  def find_category
+    @category = Category.find(params[:id])
   end
   
   def require_admin
