b0d0e3d8c360817d19171053720887b5ff79ecb1
Austin Pierce <AustinPierce@gmail.com>
2018-07-16 21:59:19 -0400

    Formatting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
   end
   
   def index
-    @articles = Article.paginate(page: params[:page], per_page: 5)
+    @articles = Article.paginate(page: params[:page], per_page: 8)
   end
   
   def destroy # action
